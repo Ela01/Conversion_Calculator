a1cf14030b254ffb07707716073b0dd8021a0ffe
Ela Wieczorek <elizabeth01wieczorek@gmail.com>
2023-05-30 18:05:07 -0400

    Cleaning up code. Will add Nav-bar and remove binding.

--- app/src/main/java/com/example/myapplication/TimeActivity.java
+++ app/src/main/java/com/example/myapplication/TimeActivity.java
@@ -9,6 +9,6 @@ public class TimeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_time);
+        setContentView(R.layout.fragment_time);
     }
 }
